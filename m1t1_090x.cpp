e4e2a78b300cc6d69a87f6d2c091f4175dfc062a
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-09-02 20:21:17 +0000

    Started m1t1

---
+++ m1t1_090x.cpp
@@ -0,0 +1,23 @@
+/*
+CSC 134
+M1T1 for the online sections 
+ramirez
+09/02/25
+Program will greet the user.
+*/
+
+// magic words
+#include <iostream>
+using  namespace  std;
+
+int main() {
+    // input: start the program
+    // processing: none 
+    // out put: 0 (no erors)
+    // special FX: Say "Hello" to the user
+
+    // Let's say hello
+    cout << "Hello, user!" << endl; // "end line" not "end 1"
+
+    return 0; // no errors
+}
