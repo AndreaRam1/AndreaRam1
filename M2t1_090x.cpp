84c6e714f090b14ee83f5ebd4639ee59e5fb5b2d
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-09-17 19:24:52 +0000

    corrections

--- M2t1_090x.cpp
+++ M2t1_090x.cpp
@@ -3,9 +3,28 @@
 // Ramirez
 // 9/17/25
 //"CheckOut Machine"
+#include <iostream>
+using namespace std;
 
 int main() {
 
     //Set Up Variables
     string first_name, last_name, full_name; // Info. Costumer
+    string product = "Avocados"; 
+    double cost_each = 0.99;
+    double total_cost;
+
+    // Costumer
+    cout << "Welcome to our " << product << " store" << endl;
+    cout << "What is your first name? ";
+    cin  >> first_name;
+    cout << "What is your last name? ";
+    cin >> last_name;
+    full_name = first_name + " " + last_name;
+    cout << "Nice to meet you, " << full_name << endl;
+
+    return 0;
+
+
+
 }
