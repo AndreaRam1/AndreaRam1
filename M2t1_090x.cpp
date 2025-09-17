c602e7b8d50b24fa7705651a0d033c344e4d5a3e
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-09-17 19:13:20 +0000

    Variables

---
+++ M2t1_090x.cpp
@@ -0,0 +1,11 @@
+//CSC 134
+//M2T1 - Product Sales 
+// Ramirez
+// 9/17/25
+//"CheckOut Machine"
+
+int main() {
+
+    //Set Up Variables
+    string first_name, last_name, full_name; // Info. Costumer
+}
