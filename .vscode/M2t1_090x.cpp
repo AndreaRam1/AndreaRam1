c553d91a52136a0602ab6b4646246b8e31a2ea79
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-09-18 18:53:11 +0000

    first part

---
+++ .vscode/M2t1_090x.cpp
@@ -0,0 +1,26 @@
+//CSC 134
+//M2t1 - Product Sale
+// Ramirez
+//09/18/25
+// "Checkout machine"
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    //variables
+    string first_name, last_name, full_name; //Costumer information
+    string product = "Avocados"; 
+    int amount_purchased;
+    double cost_each = 0.99;
+    double total_cost;
+
+    //Greet the costumer.
+    cout << "Welcome to our " << product << " store!" << endl;
+    cout << "What is your first name? ";
+    cin >> first_name:
+    cout << "What is your last name? ";
+    cin >> last_name;
+
+}
