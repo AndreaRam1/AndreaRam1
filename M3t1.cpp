853859ea5563843d34bdd4c5c915e555c7230b8d
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-09-29 19:14:31 +0000

    traying!!

---
+++ M3t1.cpp
@@ -0,0 +1,34 @@
+// CSC 134
+//M3T1
+//Ramirez
+//29/08/25
+// Area of two rectangles. 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declare variables
+    double width_1, length_1, width_2, length_2;
+    double area_1, area_2;
+
+    //Instructions
+
+    cout << " Please enter the width and length for the firts rectangle" << endl;
+    cout << " separeted by a space." << endl;
+    cin >> width_1 >> length_1;
+
+    cout << " Please enter the width and length for the second rectangle" << endl;
+    cout << " separeted by a space." << endl;
+    cin >> width_2 >> length_2;
+
+    // Answer
+    area_1 = width_1 * length_1;
+    area_2 = width_2 * length_2;
+
+    // Results
+    cout <<" Rectangle 1 has area of " << area_1 << endl;
+    cout << "Rectangle 2 has area of " << area_2 << endl;
+
+    return 0; 
+}
