9273d29bbc2b58ad376dd5c433191fe2112085f6
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-10-01 17:50:08 +0000

    Chuckie's game

--- M3LAB1.cpp
+++ M3LAB1.cpp
@@ -1,23 +1,28 @@
-//Rugrats game!! Applygin IF, ELSE and VOID Fuctions
+//CSC 134
+//M3LAB1
+//Andrea Ramirez
+// 10/01/25
+//Rugrats game!! Applyging IF, ELSE and VOID Fuctions
 //Give the user 3 options 
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-//VOID fuctions declaration (3 options Explore, Hide, Brave).
+//VOID functions declaration (3 options Explore, Hide, Brave).
 void doExplore();
 void doHide();
 void doBrave();
 
 int main() {
     string choice; //Context and instructions of the game. 
-    cout << "✨Welcome to Chukies's Adventure!!✨ " << endl;
-    cout << "🫣 Help Chukie explore a new adventure with his firends 👶👶" << endl;
+    cout << "✨Welcome to Chuckies's Adventure!!✨ " << endl;
+    cout << "🫣 Help Chuckie explore a new adventure with his friends 👶👶" << endl;
     cout << "Chuckie hears a weird noise in the kitchen 😮. What should he do?" << endl;
     cout << "Options: Explore | Hide | Brave" << endl;
     cout << "Type your choice" << endl; 
     cin >> choice;
+    // IF/ELSE conditions
 
     if (choice == "Explore"){
         doExplore();
@@ -32,8 +37,9 @@ int main() {
         cout << "The adventure is over. Thanks for helping Chuckie! 🙏" << endl;
     }
     else {
+        //If user enters something invalid
         cout << "Chuckie doesn't understand that option and gets even more nervous! 😥" << endl;
-        cout
+        cout << "Try again please! 🥹" << endl; 
     }
     
 
@@ -45,18 +51,18 @@ int main() {
 
 //  Functions
 void doExplore() {
-    cout << " Chuckie follows Tommy into the kitchen..." << endl;
+    cout << "Chuckie follows Tommy into the kitchen..." << endl;
     cout << " He steps on a toy, falls down and yells 😵" << endl;
 }
 
 void doHide() {
-    cout << "Chuckie hides behind the couch and close his eyes..." << endl;
+    cout << "Chuckie hides behind the couch and closes his eyes..." << endl;
     cout << "👫 Phil and Lil jump out shouting 'We found you!!'. Chuckie screams." << endl;
 
 }
 
 void doBrave() {
-    cout << "💪 Chuckie takes a deep breath, and say 'I can do this' 🤓" << endl;
+    cout << "💪 Chuckie takes a deep breath, and says 'I can do this' 🤓" << endl;
     cout << "He steps forward... sees a scary shadow and runs away screaming...🏃‍♂️💨" << endl;
 
 }
