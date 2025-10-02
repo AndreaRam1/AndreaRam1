e1ce1466c39e124d0d559bdba7b057b46e3792a0
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-10-02 18:43:24 +0000

    question 3

--- M2HW.cpp
+++ M2HW.cpp
@@ -31,7 +31,7 @@ int main() {
     cin >> startingBalance;
     cout << "Second enter the amount of the deposit: $";
     cin >> depositAmount;
-    cout << "Last, enter the amoun to withdraw: $";
+    cout << "Last, enter the amount to withdraw: $";
     cin >> withdrawalAmount;
 
     //Calculations
@@ -43,7 +43,54 @@ int main() {
     cout << "Name on account: " << customerName << endl;
     cout << "Account number: " << acountNumbre << endl;
     cout << "Final balance: $" << finalBalance << endl;
+
+
+    //Question #2: General Crates Update
+
+    cout << "======================================" << endl;
+    cout << "=============Question 2===============" << endl;
+    cout << "=========General Crates Update========" << endl;
+
+    /* Updating the code M2LA 
+    update values per constant */
+    
+    const double BUILD_COST_RATE = 0.30;
+    const double CUSTOMER_RATE = 0.52;
+
+    double crateLength, crateWidth, crateHeight, crateVolume, totalBuildCost, totalCharge, totalProfit;
+
+    cout << "Enter the dimensions of the crate in feet" << endl;
+    cout << "Lenght: ";
+    cin >> crateLength; 
+    cout << "Width: ";
+    cin >> crateWidth;
+    cout << "Height: ";
+    cin >> crateHeight;
+
+     //Calculations
+
+    crateVolume = crateLength * crateWidth * crateHeight; // Volume
+    totalBuildCost = crateVolume * BUILD_COST_RATE; // build cost
+    totalCharge = crateVolume * CUSTOMER_RATE; //charge
+    totalProfit = totalCharge - totalBuildCost; // profit
+
+    // Results
+
+    cout << "The volume of the crate is " << crateVolume << " cubic feet." << endl; 
+    cout << "Cost to build: $" << totalBuildCost << endl;
+    cout << "Charge to custmer: $" << totalCharge << endl;
+    cout << "Profit: $" << totalProfit << endl;
+
+    //Question #3: Pizza party
+
+    cout << "======================================" << endl;
+    cout << "=============Question 3===============" << endl;
+    cout << "============Pizza party===============" << endl;
+
     
 
+    
+
+    return 0;
 
 }
