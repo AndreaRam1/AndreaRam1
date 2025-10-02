d5ac8d34fbeb5b54c54f14dd82c206e32a7caa7a
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-10-02 16:54:16 +0000

    star..

---
+++ M2LAB.cpp
@@ -0,0 +1,8 @@
+//CSC 134
+//M2LAB
+//Andrea Ramirez
+// 10/02/25
+// This program calculates the volume, cost, charge and profit
+// In this program the user is asked to enter the crate's dimensions
+//Calculate the volume of a cube
+
