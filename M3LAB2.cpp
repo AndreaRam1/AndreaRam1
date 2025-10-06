d4252be5b7efe23222e2387fdb13d8a3e843a8d4
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-10-06 21:51:01 +0000

    corrections and ready

--- M3LAB2.cpp
+++ M3LAB2.cpp
@@ -10,4 +10,39 @@ using namespace std;
 int main() {
     // Variables 
     int grade;
+    
+    // instructins to the user: Enter a numerical grade
+
+    cout << "Please enter your numerical grade: ";
+    cin >> grade;
+
+    // Validation of the grade
+    
+    if (grade < 0 || grade > 100) {
+        cout << "Invalid grade. Please enter a number between 0 and 100." << endl;
+        return 1; // For invalid input exit te program
+    }
+    char lettergrade; // variable for the letter grade
+
+    // Make the conversion 
+    if (grade >= 90 && grade <= 100) {
+        lettergrade = 'A';
+    }
+    else if (grade >= 80 && grade <=89) {
+        lettergrade = 'B';
+    }
+    else if (grade >= 70 && grade <=79) {
+        lettergrade = 'C';
+    }
+    else if (grade >=60 && grade <=69) {
+        lettergrade = 'D';
+    }
+    else {
+        lettergrade = 'F'; //any number between 0 and 59
+    }
+
+    // Results
+    cout << "Your letter grade is: " << lettergrade << endl;
+
+    return 0;
 }
