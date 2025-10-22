044158721a6f1c041be95889af69ba6a56616a9a
Andrea Ramirez Cabello <ramireza1255@student.faytechcc.edu>
2025-10-22 21:36:28 +0000

    ;)

--- M4LAB1.cpp
+++ M4LAB1.cpp
@@ -16,6 +16,16 @@ int main() {
     cout << "Give me the width of your block:" << endl;
     cin >> width;
 
+    cout << "\n One row" << endl;
+    for ( int i = 0; i < height; i++){
+        cout << "#" << endl;
+    }
+
+    cout << "\n One column" << endl;
+    for (int j = 0; j < width; j++){
+        cout << "#" << endl;
+    }
+
     cout << "\nHere is your block 🗿: \n" << endl;
 
     for ( int i = 0; i < height; i++){  //conditions for rows (i)
